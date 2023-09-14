ab713995e3e77e253cf6832df91252c31bf409b1
rajaniraju <rajaniraju2003@gmail.com>
2023-09-14 12:16:08 -0500

    Initial Commit

---
+++ Hubs/ChatHub.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.SignalR;
+
+namespace Chat.Hubs
+{
+    public class ChatHub:Hub
+    {
+        private readonly string _botUser;
+        public ChatHub()
+        {
+            _botUser = "MyChat_Bot";
+        }
+        public async Task JoinRoom(UserConnection userConnection)
+        {
+            await Groups.AddToGroupAsync(Context.ConnectionId, userConnection.Room);
+            await Clients.Group(userConnection.Room).SendAsync("ReceiveMessage",_botUser,$"{ userConnection.User} has joined{userConnection.Room}");
+        }
+    }
+}
